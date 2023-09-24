27c9e0bd63b676898c8da5e93726754644b24321
Evgeniy Olkhin <mrjeka111@gmail.com>
2023-09-24 14:57:51 +0000

    Add loop

--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,59 @@ use hostname;
 use settings::Settings;
 
 fn main() {
-    let settings = Settings::new();
-    let _hostname: String = match hostname::get() {
-        Ok(hostname) => match hostname.into_string(){
-            Ok(hostname) => hostname,
+    loop {
+        let settings = Settings::new();
+        let _hostname: String = match hostname::get() {
+            Ok(hostname) => match hostname.into_string(){
+                Ok(hostname) => hostname,
+                Err(_) => panic!("Error with parse hostname"),
+            },
             Err(_) => panic!("Error with parse hostname"),
-        },
-        Err(_) => panic!("Error with parse hostname"),
-    };
-    println!("{:?}", settings);
-
-    // Print out our settings
-    let addresses = match settings {
-        Ok(settings) => settings.addresses,
-        Err(_) => {
-            panic!("Failed to read addresses config file")
-        }
-    };
-
-    for address in &addresses {
-        let port = match address.port {
-            Some(port) => port,
-            None => 0,
         };
-        let ports = match address.ports.clone() {
-            Some(ports) => ports,
-            None => vec![port],
+        println!("{:?}", settings);
+
+        // Print out our settings
+        let addresses = match settings {
+            Ok(settings) => settings.addresses,
+            Err(_) => {
+                panic!("Failed to read addresses config file")
+            }
         };
 
-        for _port in ports.clone() {
-            let full_address = SocketAddr::new(
-                match IpAddr::from_str(&address.host) {
-                    Ok(address) => address,
-                    Err(_) => panic!("Failed to parse ip address"),
-                },
-                _port.try_into().unwrap()
-            );
-            match TcpStream::connect_timeout(
-                &full_address, 
-                std::time::Duration::from_secs(1)
-            ) {
-                Ok(_) => println!(
-                    "{} -> {} | Соединение установлено успешно", 
-                    _hostname,
-                    full_address
-                ),
-                Err(e) => println!(
-                    "{} -> {} | Ошибка при установке соединения: {}", 
-                    _hostname,
-                    full_address, 
-                    e
-                ),
+        for address in &addresses {
+            let port = match address.port {
+                Some(port) => port,
+                None => 0,
+            };
+            let ports = match address.ports.clone() {
+                Some(ports) => ports,
+                None => vec![port],
+            };
+
+            for _port in ports.clone() {
+                let full_address = SocketAddr::new(
+                    match IpAddr::from_str(&address.host) {
+                        Ok(address) => address,
+                        Err(_) => panic!("Failed to parse ip address"),
+                    },
+                    _port.try_into().unwrap()
+                );
+                match TcpStream::connect_timeout(
+                    &full_address, 
+                    std::time::Duration::from_millis(500)
+                ) {
+                    Ok(_) => println!(
+                        "{} -> {} | Connection established successfully", 
+                        _hostname,
+                        full_address
+                    ),
+                    Err(e) => println!(
+                        "{} -> {} | Error establishing connection: {}", 
+                        _hostname,
+                        full_address, 
+                        e
+                    ),
+                }
             }
         }
     }
